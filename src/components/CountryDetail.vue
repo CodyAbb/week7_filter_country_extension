a4989a9ccf8a04ba49d665532a2153759feabb79
Cody Abbott <cody.abbott16@gmail.com>
2020-01-15 22:58:36 +0000

    can now show languages in list

--- src/components/CountryDetail.vue
+++ src/components/CountryDetail.vue
@@ -3,6 +3,12 @@
     <h2>{{country.name}}</h2>
     <p>Capital: {{country.capital}}</p>
     <p>Population: {{country.population}}</p>
+    <p>Spoken Languages:</p>
+    <ul>
+      <li v-for="language in country.languages">
+        {{language.name}}
+      </li>
+    </ul>
     <img :src="country.flag" alt="">
   </article>
 </template>
